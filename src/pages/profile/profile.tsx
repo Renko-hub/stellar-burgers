9f7da53904378130736423264157f67bef346e56
Renko <whiterockdi@gmail.com>
2025-10-29 21:30:37 +0300

    Версия 29.10.25

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,12 +1,13 @@
+// profile.tsx
+
+import { updateUser } from '../../services/slices/userSlice';
+import { useDispatch, useSelector } from '../../services/store';
 import { ProfileUI } from '@ui-pages';
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
 
 export const Profile: FC = () => {
-  /** TODO: взять переменную из стора */
-  const user = {
-    name: '',
-    email: ''
-  };
+  const { data: user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const [formValue, setFormValue] = useState({
     name: user.name,
@@ -29,6 +30,7 @@ export const Profile: FC = () => {
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(updateUser(formValue));
   };
 
   const handleCancel = (e: SyntheticEvent) => {
@@ -56,6 +58,4 @@ export const Profile: FC = () => {
       handleInputChange={handleInputChange}
     />
   );
-
-  return null;
 };
